3a6ea6c522b7e9a318b453ee6de26decaf0943e8
Djime223 <djimdemb45@gamil.com>
2022-06-17 12:01:54 +0200

    connexion a la base des données

--- src/main/java/sira/dao/ConnexionBD.java
+++ src/main/java/sira/dao/ConnexionBD.java
@@ -6,25 +6,25 @@ import java.sql.SQLException;
 
 public class ConnexionBD {
 
-    private static Connection connection;
-    private static String     url      = "jdbc:mysql://localhost:3306/projet_sira";
-    private static String     user     = "root";
-    private static String     password = "";
+	  private static Connection connection;
+	    private static String     url      = "jdbc:mysql://localhost:3306/projet_sira";
+	    private static String     user     = "root";
+	    private static String     password = "";
 
-    public static Connection getConnection() {
-        try {
-            Class.forName( "com.mysql.cj.jdbc.Driver" );
-        } catch ( ClassNotFoundException e ) {
-            System.out.println( "classe " + e.getMessage() );
-        }
+	    public static Connection getConnection() {
+	        try {
+	            Class.forName( "com.mysql.cj.jdbc.Driver" );
+	        } catch ( ClassNotFoundException e ) {
+	            System.out.println( "classe " + e.getMessage() );
+	        }
 
-        try {
-            connection = DriverManager.getConnection( url, user, password );
-        } catch ( SQLException e ) {
-            System.out.println( "getConnexion : " + e.getMessage() );
-        }
+	        try {
+	            connection = DriverManager.getConnection( url, user, password );
+	        } catch ( SQLException e ) {
+	            System.out.println( "getConnexion : " + e.getMessage() );
+	        }
 
-        return connection;
-    }
+	        return connection;
+	    }
 
 }
